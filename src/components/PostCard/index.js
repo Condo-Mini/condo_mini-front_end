eabc663a9cf478efa1097772f34fc31b70de9164
¨WesleyFernandoAlmeida¨ <wesley.fer.almeida98@gmail.com>
2022-06-29 23:23:32 -0300

    commit

--- src/components/PostCard/index.js
+++ src/components/PostCard/index.js
@@ -1,9 +1,40 @@
 import React from 'react';
+import { makeStyles } from '@material-ui/styles';
+import { Card, CardHeader, CardContent, CardActions, Typography, Avatar } from '@mui/material';
+import './style.css';
+const useStyles = makeStyles({
+  root: {
+    padding: 1,
+  },
+});
 
 function PostCard({ post }) {
+  const classes = useStyles();
+  
   return (
-   <h1>{post.description}</h1>
+   <Card className={classes.root}>
+     <CardHeader
+       avatar={<Avatar src={post.author.avatar}/>}
+       title={<Typography variant="h6">{post.author.name}</Typography>}
+       subheader={
+         <div className="subheader">Aqui
+           <Typography variant="caption">
+             { post.author.name }
+           </Typography>
+           <Typography variant="subtitle2">
+             { post.author.name }
+           </Typography>
+         </div>
+       }
+     />
+     <CardContent>
+
+     </CardContent>
+     <CardActions>
+
+     </CardActions>
+   </Card>
   )
-}
+}  
 
 export default PostCard;
